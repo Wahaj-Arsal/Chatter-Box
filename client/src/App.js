71746c20d39e24dcf97a467f80288af6cbced3d5
Wahaj-Arsal <83618649+Wahaj-Arsal@users.noreply.github.com>
2022-08-28 23:37:47 +0100

    socket working between two people

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 /** @format */
 
+import { useEffect } from "react";
+
+import { io } from "socket.io-client";
+const socket = io.connect("http://localhost:8080");
+
 function App() {
-  return <h1>Hello App</h1>;
+  const sendMessage = () => {
+    socket.emit("sendMessage", { message: "Hello" });
+  };
+
+  useEffect(() => {
+    socket.on("receiveMessage", (data) => {
+      alert(data.message);
+    });
+  }, [socket]);
+
+  return (
+    <>
+      <h1>Chatter-Box</h1>
+      <h2>Home Page</h2>
+      <input placeholder="message" />
+      <button onClick={sendMessage}>Send Message</button>
+    </>
+  );
 }
 
 export default App;
---
+++ server.js
@@ -0,0 +1,35 @@
+/** @format */
+
+const PORT = 8080;
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log("Home Page");
+});
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("sendMessage", (data) => {
+    // console.log(data);
+    socket.broadcast.emit("receiveMessage", data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
